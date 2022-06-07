767f1f6f1811c67aa7419d9e78578322151bd864
giovandrofabio <giovandrofabiosantos@hotmail.com>
2022-06-07 10:00:58 -0300

    Melhorias do Back

--- app/Http/Controllers/AdocaoController.php
+++ app/Http/Controllers/AdocaoController.php
@@ -2,17 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Rules\AdocaoUnicaPet;
+use App\Http\Resources\AdocaoCollection;
 use App\Models\Adocao;
 use Illuminate\Http\Request;
 
 class AdocaoController extends Controller
-{
+{   
+    public function index()
+    {
+        $adocoes = Adocao::with('pet')->get();
+
+        return new AdocaoCollection($adocoes);
+    }
+
+    /**
+     * Cria um novo registro de adoção
+     *
+     * @param Request $request
+     * @return Adocao
+     */
     public function store(Request $request)
     {
         $request->validate([
-            "email" => ['required','email'],
+            "email" => ['required','email', new AdocaoUnicaPet($request->input('pet_id',0))],
             "valor" => ['required','numeric', 'between:10,100'],
-            "pet_id" => ['required','int']         
+            "pet_id" => ['required','int','exists:pets,id']         
         ]);
 
         $dadosDaAdocao = $request->all();
